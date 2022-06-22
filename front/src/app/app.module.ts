2ec9a27c555cbc5b3b9f9d267c40af089ec5bb1a
Lucas Zawadneak <lucaszawadneak@gmail.com>
2022-06-22 20:42:15 -0300

    feat:register page

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -1,27 +1,28 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { FeatherModule } from 'angular-feather';
-import { allIcons } from 'angular-feather/icons';
-import { FormsModule } from '@angular/forms'
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { FeatherModule } from "angular-feather";
+import { allIcons } from "angular-feather/icons";
+import { FormsModule } from "@angular/forms";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { FullComponent } from './layouts/full/full.component';
-import { DemoFlexyModule } from './demo-flexy-module'
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { FullComponent } from "./layouts/full/full.component";
+import { DemoFlexyModule } from "./demo-flexy-module";
 
 // Modules
-import { DashboardModule } from './dashboard/dashboard.module';
-import { ComponentsModule } from './components/components.module';
-import { LoginComponent } from './layouts/login/login.component';
-import { RegisterComponent } from './layouts/register/register.component';
+import { DashboardModule } from "./dashboard/dashboard.module";
+import { ComponentsModule } from "./components/components.module";
+import { LoginComponent } from "./layouts/login/login.component";
+import { RegisterComponent } from "./layouts/register/register.component";
+import { IMaskModule } from "angular-imask";
 
 @NgModule({
   declarations: [
     AppComponent,
     FullComponent,
     LoginComponent,
-    RegisterComponent
+    RegisterComponent,
   ],
   imports: [
     BrowserModule,
@@ -31,9 +32,10 @@ import { RegisterComponent } from './layouts/register/register.component';
     DemoFlexyModule,
     DashboardModule,
     ComponentsModule,
-    FormsModule
+    FormsModule,
+    IMaskModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- front/src/app/layouts/register/register.component.ts
+++ front/src/app/layouts/register/register.component.ts
@@ -1,15 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import IMask from "imask";
 
 @Component({
-  selector: 'app-register',
-  templateUrl: './register.component.html',
-  styleUrls: ['./register.component.scss']
+  selector: "app-register",
+  templateUrl: "./register.component.html",
+  styleUrls: ["./register.component.scss"],
 })
 export class RegisterComponent implements OnInit {
+  cpfMask = { mask: "000.000.000-75" };
+  cepMask = { mask: "00000-000" };
+  numberMask = { mask: IMask.MaskedNumber };
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
