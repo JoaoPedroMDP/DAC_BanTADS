5a7db60bb4d47b0d77d91b74b832d1f4b7bd68e9
JoaoPedroMDP1 <dev.jpmpjp00@gmail.com>
2022-08-06 21:22:32 -0300

    [change] Transferência e Saque também estão com números formatados

--- front/src/app/cliente/transactions/withdrawal/withdrawal.component.ts
+++ front/src/app/cliente/transactions/withdrawal/withdrawal.component.ts
@@ -6,15 +6,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./withdrawal.component.css']
 })
 export class WithdrawalComponent implements OnInit {
-  
+  withdrawalValue!: number;
+
   constructor() { }
 
   ngOnInit(): void {
+    this.withdrawalValue = 0;
   }
 
-  sacar(value: string): void {
-    let parsed = Number(value);
-    console.log(`Sacando ${parsed}`);
+  withdraw(): void {
+    alert(`Sacando ${this.withdrawalValue}`);
 
     // TODO: verificar se a quantia existe
     // TODO: sacar a quantia
