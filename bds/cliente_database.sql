c79983313b81caca40dc266b81e0961468e2e805
Lucas Cassilha <lucaszawadneak@gmail.com>
2022-08-18 14:44:57 -0300

    sql cliente

--- bds/cliente_database.sql
+++ bds/cliente_database.sql
@@ -1,6 +1,19 @@
-CREATE TABLE IF NOT EXISTS login (
+CREATE TABLE IF NOT EXISTS endereco (
     id BIGINT PRIMARY KEY,
-    password TEXT NOT NULL,
+    rua VARCHAR(255) NOT NULL,
+    numero VARCHAR(255) NOT NULL,
+    complemento varchar(255),
+    cep varchar(255) NOT NULL,
+    cidade varchar(255) NOT NULL,
+    estado varchar(255) NOT NULL
+);
+
+
+CREATE TABLE IF NOT EXISTS cliente (
+    id BIGINT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
     email VARCHAR(255) NOT NULL,
-    user_id INT NOT NULL
+    cpf varchar(11) NOT NULL,
+    endereco BIGINT,
+    CONSTRAINT fk_endereco FOREIGN KEY(endereco) REFERENCES endereco(id)
 );
