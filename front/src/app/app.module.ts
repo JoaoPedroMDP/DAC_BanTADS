afd44a72c76de2ddcc1a5af4a2655ba349b11dce
João Pedro Martins de Paula <34918112+JoaoPedroMDP@users.noreply.github.com>
2022-09-11 16:03:05 +0000

    [change] Agora "extrato" está chamando o microserviço de conta. Deverá chamar, posteriormente, o api gateway [fix] Host check do angular desabilitado [fix] Módulos duplicados angular [change] Faltou um yarn no ultimo commit

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -1,8 +1,6 @@
 import { HttpClientModule } from "@angular/common/http";
 import { NgModule } from "@angular/core";
-import { FormsModule } from "@angular/forms";
 import { MatSortModule } from "@angular/material/sort";
-import { MatTableModule } from "@angular/material/table";
 import { BrowserModule } from "@angular/platform-browser";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 
