b55195e3db48af1e4eeee7c8c7ea4f39fb21d518
João Pedro Martins de Paula <34918112+JoaoPedroMDP@users.noreply.github.com>
2022-09-12 20:26:30 +0000

    [add] ModelMapper no MQ de account

--- back/account/src/main/java/com/bantads/account/account/amqp/account/AccountQueueConfiguration.java
+++ back/account/src/main/java/com/bantads/account/account/amqp/account/AccountQueueConfiguration.java
@@ -1,10 +1,16 @@
 package com.bantads.account.account.amqp.account;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.support.converter.DefaultClassMapper;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.amqp.support.converter.MessageConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import com.bantads.account.account.amqp.AccountTransfer;
 
 @Configuration
 public class AccountQueueConfiguration {
@@ -25,7 +31,21 @@ public class AccountQueueConfiguration {
     }
 
     @Bean
+    public DefaultClassMapper classMapper() {
+        DefaultClassMapper classMapper = new DefaultClassMapper();
+        Map<String, Class<?>> idClassMapping = new HashMap();
+        classMapper.setTrustedPackages("*");
+        idClassMapping.put("com.bantads.saga.amqp.AccountTransfer", AccountTransfer.class);
+        classMapper.setIdClassMapping(idClassMapping);
+
+        return classMapper;
+    }
+
+    @Bean
     public MessageConverter aJsonMessageConverter() {
-        return new Jackson2JsonMessageConverter();
+        Jackson2JsonMessageConverter converter = new Jackson2JsonMessageConverter();
+        converter.setClassMapper(classMapper());
+
+        return converter;
     }
 }
